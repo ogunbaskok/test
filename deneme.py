01d89a6df385cbf0b45a6e96ded3d93e2d4cb7d1
ogün <ogunbaskok52@gmail.com>
2022-01-27 00:57:45 +0300

    test

---
+++ deneme.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+PATH = "C:\Program Files\chromedriver\chromedriver1.exe"
+
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://google.com.tr")
+time.sleep(5)
+driver.maximize_window()
+time.sleep(5)
+    
+   
